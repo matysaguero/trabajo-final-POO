f3c4bdfb129ca1e81e6d1b17b45c3f1d9f71ce4a
Mariano Feito <marianofeito12@gmail.com>
2026-08-21 20:14:40 -0300

    Agrega validaciones a Ingresante

--- src/Ingresante.java
+++ src/Ingresante.java
@@ -9,6 +9,21 @@ public abstract class Ingresante {
     protected ArrayList<Documento> documentos;    
 
     public Ingresante(String nombre_declarado, int altura_visual, int peso_en_balanza, String clan, String ciudad_origen, ArrayList<Documento> documentos){
+        if (nombre_declarado == null || nombre_declarado.isEmpty()){
+            throw new IllegalArgumentException("el nombre declarado no puede ser nulo ni vacio");
+        }
+        if (altura_visual <= 0){
+            throw new IllegalArgumentException("la altura declarada no puede ser cero ni negativa");
+        }
+        if (peso_en_balanza <= 0){
+            throw new IllegalArgumentException("el peso no puede ser cero ni negativo");
+        }
+        if (clan == null || clan.isEmpty()){
+            throw new IllegalArgumentException("el clan no puede ser nulo ni vacio");
+        }
+        if (ciudad_origen == null || ciudad_origen.isEmpty()){
+            throw new IllegalArgumentException("ciudad_origen no puede ser nulo ni vacio");
+        }
         this.nombre_declarado = nombre_declarado;
         this.altura_visual = altura_visual;
         this.peso_en_balanza = peso_en_balanza;
@@ -41,10 +56,5 @@ public abstract class Ingresante {
     public abstract String darPresentacion();
     public abstract String responderInterrogatorio();
 
-    public Ingresante (){
-    }
-
-    public void Coartada(){
-
-    }
 }
+
