4b48272b6ed9021f57865091dff7ebc1d7c28e43
Matías Agüero <matysaguero@gmail.com>
2026-08-21 20:09:20 -0300

    la validacion de pasaporte y eliminar comportamientos vacios de jugador

--- src/Pasaporte.java
+++ src/Pasaporte.java
@@ -3,6 +3,21 @@ public class Pasaporte extends Documento {
     protected int altura;
     protected String pais;
     protected String ciudad;
-    protected String fechaVencimiento;
+
+    public Pasaporte(int fecha_vencimiento, String emisor, String foto, int num_id, int peso, int altura, String pais, String ciudad, String fechaVencimiento){
+        super(fecha_vencimiento, emisor, foto, num_id);
+        if (peso <= 0) {
+            throw new IllegalArgumentException("Peso no valido");
+        }
+        if (altura <= 0){
+            throw new IllegalArgumentException("Altura no valida");
+        }
+        if (pais == null || pais.isBlank()){
+            throw new IllegalArgumentException("Pais no valido");
+        }
+        if (ciudad == null || ciudad.isBlank()){
+            throw new IllegalArgumentException("Ciudad no valida");
+        }
+    }
 
 }
