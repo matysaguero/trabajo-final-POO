d4626d2792ced8cf5ac2eae57dfe5e3c9be0f63b
Luly2005 <vasquezmariadelourdes05@gmail.com>
2026-09-08 13:26:38 -0300

    Se realizo el constructor, getters y metodos a la clase Jugador

--- src/controlador/Jugador.java
+++ src/controlador/Jugador.java
@@ -1,6 +1,44 @@
 package controlador;
 
 public class Jugador{
+    private String nombre;
     private int reputacionJugador;
 
+    public Jugador (String nombre, int reputacionInicial){
+        if (nombre == null || nombre.isBlank()) {
+            throw new IllegalArgumentException("El nombre del jugador debe ser obligatorio.");
+        }
+        if (reputacionInicial <0) {
+            throw new IllegalArgumentException("La reputacion no puede ser negativa.");
+
+        }
+
+        this.nombre = nombre;
+        this.reputacionJugador = 3;
+
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getReputacion(){
+        return reputacionJugador;    
+    }
+
+    public boolean aceptar(){
+        return true;
+    }
+
+    public boolean rechazar(){
+        return false;
+    }
+
+    public void sumaReputacion(){
+        reputacionJugador++;
+    }
+
+    public void restaReputacion (){        
+        reputacionJugador--;
+    }
 }
